1e312b2c5b52a57b0106014a38d09f51e18cc17b
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-05-12 11:39:18 +0200

    add relative path

--- moq-api/src/server.rs
+++ moq-api/src/server.rs
@@ -187,13 +187,14 @@ fn origin_key(namespace: &str) -> String {
 }
 
 fn parse_topology(content: &str) -> Result<HashMap<String, String>, AppError> {
-    let node_map = read_graph();
+    let node_map = read_graph(content);
     return Ok(node_map);
 }
 
-fn read_graph() -> HashMap<String, String> {
+fn read_graph(content: &str) -> HashMap<String, String> {
+    // TODO: import file dynamically, currently not possible with this petgraph version
     let graph: petgraph::graph::Graph<_, _> = petgraph::dot::dot_parser::graph_from_file!(
-        "/home/collybita/repos/moq-rs-topo/target/debug/topo.dot"
+        "./target/debug/topo.dot"
     );
 
     let mut map: HashMap<String, String> = HashMap::new();
