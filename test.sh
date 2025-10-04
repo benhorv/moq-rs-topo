64e0b0dcbf2604ecab9ac825a50df9df9b6d347a
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-10-04 21:20:30 +0200

    clean up remains of moq-dir

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 
 # --- Port Definitions ---
 PORT_REDIS=6379
-PORT_MOQ_DIR=4444
 PORT_MOQ_API=8080
 PORT_RELAY1=4451
 PORT_RELAY2=4452
@@ -26,31 +25,27 @@ SLEEP_DURATION=0.5
 xterm -T "Redis Server" -e "bash -c \"echo 'Working directory: \$(pwd)'; echo 'Starting: redis-server'; redis-server; exec bash\"" &
 sleep $SLEEP_DURATION
 
-# 2. moq-dir
-xterm -T "moq-dir (P:$PORT_MOQ_DIR)" -e "bash -c \"cd moq-dir && echo 'Working directory: \$(pwd)'; echo 'Starting: moq-dir on port $PORT_MOQ_DIR'; RUST_LOG=debug cargo run --bin moq-dir -- --bind '[::]:$PORT_MOQ_DIR' --tls-cert ../dev/localhost.crt --tls-key ../dev/localhost.key; exec bash\"" &
-sleep $SLEEP_DURATION
-
-# 3. moq-api
+# 2. moq-api
 xterm -T "moq-api (P:$PORT_MOQ_API)" -e "bash -c \"cd moq-api && echo 'Working directory: \$(pwd)'; echo 'Starting: moq-api on port $PORT_MOQ_API (Redis: $PORT_REDIS, Topo: $TOPO_FILE_PATH)'; RUST_LOG=debug cargo run --bin moq-api -- --bind '[::]:$PORT_MOQ_API' --redis 'redis://localhost:$PORT_REDIS/' --topo '$TOPO_FILE_PATH'; exec bash\"" &
 sleep $SLEEP_DURATION
 
-# 4. Relay 1
-xterm -T "Relay 1 (P:$PORT_RELAY1)" -e "bash -c \"cd moq-relay-ietf && echo 'Working directory: \$(pwd)'; echo 'Starting: Relay 1 on port $PORT_RELAY1'; RUST_LOG=debug cargo run --bin moq-relay-ietf -- --bind '[::]:$PORT_RELAY1' --tls-cert ../dev/localhost.crt --tls-key ../dev/localhost.key --tls-disable-verify --api http://localhost:$PORT_MOQ_API --node https://localhost:$PORT_RELAY1 --dev --announce https://localhost:$PORT_MOQ_DIR/udp; exec bash\"" &
+# 3. Relay 1
+xterm -T "Relay 1 (P:$PORT_RELAY1)" -e "bash -c \"cd moq-relay-ietf && echo 'Working directory: \$(pwd)'; echo 'Starting: Relay 1 on port $PORT_RELAY1'; RUST_LOG=debug cargo run --bin moq-relay-ietf -- --bind '[::]:$PORT_RELAY1' --tls-cert ../dev/localhost.crt --tls-key ../dev/localhost.key --tls-disable-verify --api http://localhost:$PORT_MOQ_API --node https://localhost:$PORT_RELAY1 --dev; exec bash\"" &
 sleep $SLEEP_DURATION
 
-# 5. Relay 2
-xterm -T "Relay 2 (P:$PORT_RELAY2)" -e "bash -c \"cd moq-relay-ietf && echo 'Working directory: \$(pwd)'; echo 'Starting: Relay 2 on port $PORT_RELAY2'; RUST_LOG=debug cargo run --bin moq-relay-ietf -- --bind '[::]:$PORT_RELAY2' --tls-cert ../dev/localhost.crt --tls-key ../dev/localhost.key --tls-disable-verify --api http://localhost:$PORT_MOQ_API --node https://localhost:$PORT_RELAY2 --dev --announce https://localhost:$PORT_MOQ_DIR/udp; exec bash\"" &
+# 4. Relay 2
+xterm -T "Relay 2 (P:$PORT_RELAY2)" -e "bash -c \"cd moq-relay-ietf && echo 'Working directory: \$(pwd)'; echo 'Starting: Relay 2 on port $PORT_RELAY2'; RUST_LOG=debug cargo run --bin moq-relay-ietf -- --bind '[::]:$PORT_RELAY2' --tls-cert ../dev/localhost.crt --tls-key ../dev/localhost.key --tls-disable-verify --api http://localhost:$PORT_MOQ_API --node https://localhost:$PORT_RELAY2 --dev; exec bash\"" &
 sleep $SLEEP_DURATION
 
-# 6. Relay 3
-xterm -T "Relay 3 (P:$PORT_RELAY3)" -e "bash -c \"cd moq-relay-ietf && echo 'Working directory: \$(pwd)'; echo 'Starting: Relay 3 on port $PORT_RELAY3'; RUST_LOG=debug cargo run --bin moq-relay-ietf -- --bind '[::]:$PORT_RELAY3' --tls-cert ../dev/localhost.crt --tls-key ../dev/localhost.key --tls-disable-verify --api http://localhost:$PORT_MOQ_API --node https://localhost:$PORT_RELAY3 --dev --announce https://localhost:$PORT_MOQ_DIR/udp; exec bash\"" &
+# 5. Relay 3
+xterm -T "Relay 3 (P:$PORT_RELAY3)" -e "bash -c \"cd moq-relay-ietf && echo 'Working directory: \$(pwd)'; echo 'Starting: Relay 3 on port $PORT_RELAY3'; RUST_LOG=debug cargo run --bin moq-relay-ietf -- --bind '[::]:$PORT_RELAY3' --tls-cert ../dev/localhost.crt --tls-key ../dev/localhost.key --tls-disable-verify --api http://localhost:$PORT_MOQ_API --node https://localhost:$PORT_RELAY3 --dev; exec bash\"" &
 sleep $SLEEP_DURATION
 
-# 7. Publisher
+# 6. Publisher
 xterm -T "Publisher (to P:$PORT_RELAY1)" -e "bash -c \"echo 'Working directory: \$(pwd)'; echo 'Starting: Publisher (PORT $PORT_RELAY1)'; PORT=$PORT_RELAY1 ./dev/pub; exec bash\"" &
 sleep $SLEEP_DURATION
 
-# 8. Subscriber
+# 7. Subscriber
 xterm -T "Subscriber (from P:$PORT_RELAY3)" -e "bash -c \"echo 'Working directory: \$(pwd)'; echo 'Starting: Subscriber (PORT $PORT_RELAY3)'; PORT=$PORT_RELAY3 ./dev/sub; exec bash\"" &
 
 echo ""
